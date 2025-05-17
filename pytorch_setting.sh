e26e29060b2ca4745769aa3698064459f79135d3
j-wye <4mimprove@gmail.com>
2025-05-18 01:15:41 +0900

    0518

--- pytorch_setting.sh
+++ pytorch_setting.sh
@@ -1,8 +1,2 @@
-sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libopenblas-dev libavcodec-dev libavformat-dev libswscale-dev
-cd
-git clone --branch v0.18.0 https://github.com/pytorch/vision torchvision
-cd torchvision
-export BUILD_VERSION=0.18.0
-python3 setup.py install --user
-cd ../
-pip install 'pillow<7'
+
+
